317be9612f75d8fbd65c36da395ba7c63712ba1a
m-anas-j <jawad1anas@gmail.com>
2018-11-08 05:18:30 +0600

    Minor changes

--- src/main/java/com/webproject/dao/ProductDAOImpl.java
+++ src/main/java/com/webproject/dao/ProductDAOImpl.java
@@ -4,11 +4,17 @@ import com.webproject.entity.ProductTableEntity;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 import java.util.ArrayList;
 
 public class ProductDAOImpl implements ProductDAO {
+
+    private static StandardServiceRegistry standardServiceRegistry;
+    private static SessionFactory sessionFactory;
 
 
     public ArrayList<ProductTableEntity> getAllProducts() {
@@ -19,9 +25,18 @@ public class ProductDAOImpl implements ProductDAO {
 
         newProduct.setId(123);
         //session.save(newProduct);
-        Configuration con = new Configuration().configure().addAnnotatedClass(ProductTableEntity.class);
-        SessionFactory sf = con.buildSessionFactory();
-        Session session = sf.openSession();
+        //Configuration con = new Configuration().configure().addAnnotatedClass(ProductTableEntity.class);
+        try {
+            standardServiceRegistry = new StandardServiceRegistryBuilder().configure().build();
+            MetadataSources metadataSources = new MetadataSources(standardServiceRegistry);
+            Metadata metadata = metadataSources.getMetadataBuilder().build();
+            sessionFactory = metadata.getSessionFactoryBuilder().build();
+        }catch (Exception e)
+        {
+            System.out.println(e);
+        }
+        //SessionFactory sf = con.buildSessionFactory();
+        Session session = sessionFactory.openSession();
 
         Transaction tx = session.beginTransaction();
         session.save(newProduct);
